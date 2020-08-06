7608aa10ac5f4016e094ab031d6fd55a1e2eb15b
kyledexperience <kyledexperience@gmail.com>
2020-08-06 10:18:45 -0400

    added first line

--- src/Midterm.java
+++ src/Midterm.java
@@ -2,6 +2,11 @@
 public class Midterm {
 
 	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an "
+				+ "immortal named Kai-Yun and his trusty band of ninja monkeys.");
 
 	}
 
